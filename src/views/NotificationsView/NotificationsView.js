f3effe56e5491389280483d8e1fbe99fb1c9ee51
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-13 12:12:45 -0400

    updated the notifications view to render the Notifications components

--- src/views/NotificationsView/NotificationsView.js
+++ src/views/NotificationsView/NotificationsView.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import { findAllNotifications } from '../../services/notifications-service';
-import { useSelector } from 'react-redux';
+import { findNotificationsForUser, findAllNotifications } from '../../services/notifications-service';
+
+import React, {useEffect, useState} from 'react';
+import Notifications from "../../components/Notifications/index.js";
+import {useSelector} from "react-redux";
 
 
 const NotificationsView = () => {
-  const authUser = useSelector((state) => state.user.data);
-  const notifications = findAllNotifications(authUser.id);
-  return (
-    <div className='list-group'>
-      <h1>Notifications </h1>
-      {notifications.map((not) => {
-        return (
-          <p>
-            "Hello"
-          </p>
-        );
-      })}
-    </div>
-  )
+    const [notifications, setNotifications] = useState([]);
+    //const [error, setError] = useState();
+    const [setError] = useState();
 
+    const authUser = useSelector((state) => state.user.data);
+
+    const findMyNotifications = async () => {
+        const res = await findNotificationsForUser(authUser.id);
+        if (res.error) {
+            return setError(
+                'We ran into an issue showing your conversations. Please try again later.'
+            );
+        }
+        setNotifications(res);
+    };
+    useEffect(() => {
+        findMyNotifications();
+    }, []);
+    return (
+        <div>
+            <h1>Notifications</h1>
+            <Notifications notifications={notifications}/>
+        </div>
+    );
 };
 export default NotificationsView;
+// const NotificationsView = () => {
+//   const authUser = useSelector((state) => state.user.data);
+//   const notifications = findAllNotifications(authUser.id);
+//   return (
+//     <div className='list-group'>
+//       <h1>Notifications </h1>
+//       <li>
+//         {notifications[0].id}
+//       </li>
+//     </div>
+//   )
 
-{/* // return (
-  // <div>
-  //   <h1>
-  //     "Notifications"
-  //   </h1>
-  // <li>
-  // "Hello"  </li>
-  // </div>
-  // ) */}
+// };
+// export default NotificationsView;
