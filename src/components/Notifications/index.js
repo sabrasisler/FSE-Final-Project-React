f51b01f7409ba25fcb9010eae27567236413db75
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-13 12:11:36 -0400

    added a component to view a list of notifications

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 //import './Notification.css';
-//import Notification from './Notification';
+import Notification from './notification.js';
+import { markNotificationAsRead } from '../../services/notifications-service.js';
+
 /**
  * A container to display a list of notifications.
  */
-const Notification = ({ notifications }) => {
-  return (
-    <div>
-      <ul className='ttr-notifications list-group'>
-        {notifications &&
-          notifications.map((notifications) => {
-              return <p> Hello </p>
-            //return tuit ? <Tuit key={tuit.id} tuitFromList={tuit} /> : null;
-          })}
-      </ul>
-    </div>
-  );
+const Notifications = ({ notifications }) => {
+    return (
+        <div>
+            <ul className='ttr-notifications list-group'>
+                {notifications &&
+                    notifications.map((notification) => {
+                        return notification ? <Notification key={notification.id} notificationFromList={notification} /> : null;
+                    })}
+            </ul>
+        </div>
+    );
 };
 
-export default Tuits;
+export default Notifications;
+
+
