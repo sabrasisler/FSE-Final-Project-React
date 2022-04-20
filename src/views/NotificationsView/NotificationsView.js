e99b58fc776112ce645fde202fcb64c9ffe30999
SpencerSolomon <spencersolomon6@gmail.com>
2022-04-19 21:59:41 -0400

    Added socket listener on Notifications page for new notifications

--- src/views/NotificationsView/NotificationsView.js
+++ src/views/NotificationsView/NotificationsView.js
@@ -3,6 +3,7 @@ import { findNotificationsForUser, findAllNotifications } from '../../services/n
 import React, {useEffect, useState} from 'react';
 import Notifications from "../../components/Notifications/index.js";
 import {useSelector} from "react-redux";
+import { socket } from '../../services/socket-config';
 
 /**
  * Creates a page that displays all of the notifications for a given user 
@@ -23,9 +24,20 @@ const NotificationsView = () => {
         }
         setNotifications(res);
     };
-    useEffect(() => {
+
+    const listenForNewFollowsOnSocket = async () => {
+        socket.emit('JOIN_ROOM'); // Server will assign room for user based on session.
+        socket.on('NEW_FOLLOW', () => {
+        // when a new message is emitted to the room
+        console.log('new follow from server!');
         findMyNotifications();
-    }, []);
+    });
+    };
+
+
+    useEffect(() => {
+        listenForNewFollowsOnSocket();
+    });
     return (
         <div>
             <h1>Notifications</h1>
