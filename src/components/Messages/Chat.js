be94f019ba33254f1e108ff36f77fe891bf76a8e
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-04-20 21:02:08 -0400

    Render messages in each conversation

--- src/components/Messages/Chat.js
+++ src/components/Messages/Chat.js
@@ -1,93 +1,113 @@
-import React, {useEffect, useState} from "react";
-import * as service from '../../services/messages-service';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
+import {
+    findMessagesByConversationThunk,
+    sendMessageThunk,
+} from '../../redux/messageThunks';
+import {Link} from 'react-router-dom';
+import {FormInput} from '../../forms';
+import {socket} from '../../services/socket-config';
 import {ChatFeed} from "react-chat-ui";
-import {AlertBox} from "../../components";
-import {useParams} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {AlertBox} from "../index";
 
 /**
- * Component to render messages that are part of a conversation.
- * @returns {JSX.Element}
+ * Displays the active chat window with all its messages and send message textbox.
+ * @param {string} conversationId the id of the conversation
+ *
  */
-const Chat = () => {
-    const {uid, cid} = useParams();
-    const [message, setMessage] = useState([]);
-    const [sentMessage, setSentMessage] = useState([]);
-    const [messages, setMessages] = useState([]);
-    const [error, setError] = useState();
-    const authUser = useSelector((state) => state.user.data);
+const Chat = ({conversationId}) => {
+    const dispatch = useDispatch();
+    const userId = useSelector((state) => state.user.data.id);
+    const participants = useSelector(
+        (state) => state.messages.activeChat.participants
+    );
+    let messages = useSelector((state) => state.messages.activeChat.messages);
+    const [newMessageBody, setNewMessageBody] = useState({
+        sender: userId,
+        conversationId,
+        message: '',
+    });
 
-    const createMessage = async (message) => {
-        if (!message) return;
-        const res = await service.createMessage(authUser.id, cid, message);
-        if (res.error) {
-            return setError(
-                'We ran into an issue sending your message. Please try again later.'
-            );
-        }
-        setSentMessage(true);
-        setMessage('');
+    const listenForNewMessagesOnSocket = () => {
+        socket.emit('JOIN_ROOM');
+        socket.on('NEW_MESSAGE', () => {
+            // Make a REST post request to update inbox and active chat.
+            dispatch(findMessagesByConversationThunk(conversationId));
+        });
     };
 
     useEffect(() => {
-        const findMyMessages = async () => {
-            const res = await service.findAllMessagesByConversation(uid, cid);
-            if (res.error) {
-                return setError(
-                    'We ran into an issue showing your messages. Please try again later.'
-                );
-            }
-            // Mark sender and receiver bubbles
-            res.forEach((message) => {
-                message._id = message.id;
-                message.id = authUser.id === message.sender ? 0 : 1;
-            });
-            setMessages(res);
-        };
-        findMyMessages();
+        listenForNewMessagesOnSocket();
+        dispatch(findMessagesByConversationThunk(conversationId));
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [sentMessage]);
+    }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        dispatch(sendMessageThunk(newMessageBody));
+    };
 
     return (
         <div>
-            <h1>Messages</h1>
-            {error && <AlertBox message={error}/>}
+            <Link
+                to={`/messages`}
+                className='mt-2 me-2 btn btn-large btn-primary border border-secondary fw-bolder rounded-pill'
+            >
+                <i className='fa-solid fa-circle-arrow-left text-white fs-6'></i>
+                <span>Back to Inbox</span>
+            </Link>
+            <p className={"mt-4 mb-4"}>
+                Participants:
+                {participants.map((participant) => (
+                    <span className='badge rounded-pill bg-light' key={participant.id}>
+            {participant.name || participant.firstName}
+          </span>
+                ))}
+            </p>
+            {messages.length < 1 && <AlertBox message={'No messages.'}/>}
             {messages &&
                 <ChatFeed
                     messages={messages} // Boolean: list of message objects
                     maxHeight="500px"
                     hasInputField={false} // Boolean: use our input, or use your own
-                    showSenderName // show the name of the user who sent the message
+                    showSenderName={true} // show the name of the user who sent the message
                     bubblesCentered={true} //Boolean should the bubbles be centered in the feed?
                     // JSON: Custom bubble styles
                     bubbleStyles={{
                         text: {
-                            fontSize: 16
+                            fontSize: 16,
                         },
                         chatbubble: {
                             borderRadius: 30,
-                            padding: 20
-                        }
+                            padding: 20,
+                            backgroundColor: "gray"
+                        },
+                        userBubble: {
+                            backgroundColor: "#2a9fd6",
+                        },
                     }}
                 />}
-            <textarea
-                onChangeCapture={(e) => {
-                    setMessage(e.target.value);
-                    setSentMessage(false);
-                }}
-                placeholder="Enter message"
-                className='w-100 border-0'
-                value={message}
-            ></textarea>
-            <div className="col-2">
+
+            <form onSubmit={(e) => sendMessage(e)}>
+                <FormInput
+                    label='Send'
+                    placeholder='Enter Message'
+                    className='mt-4 w-100 border-0'
+                    value={newMessageBody.message}
+                    onChange={(e) =>
+                        setNewMessageBody({...newMessageBody, message: e.target.value})
+                    }
+                />
                 <button
-                    onClick={() => createMessage(message)}
-                    className={`btn btn-primary rounded-pill fa-pull-right fw-bold ps-4 pe-4`}>
+                    type='submit'
+                    className={`mt-3 btn btn-primary rounded-pill fa-pull-right fw-bold ps-4 pe-4`}
+                >
                     Send
                 </button>
-            </div>
+            </form>
         </div>
     );
-}
+};
 
 export default Chat;
