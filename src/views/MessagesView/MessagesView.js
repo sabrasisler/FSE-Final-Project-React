b3caf8e45397fb1d3d79d0891c77dbe4f95b344b
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-04-20 20:59:10 -0400

    Fetch conversations and pass to Conversations component

--- src/views/MessagesView/MessagesView.js
+++ src/views/MessagesView/MessagesView.js
@@ -1,37 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import * as service from '../../services/messages-service';
-import { AlertBox } from '../../components';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Routes, Route} from 'react-router-dom';
 import Conversations from '../../components/Messages/Conversations';
-import { useSelector } from 'react-redux';
+import Chat from '../../components/Messages/Chat';
+import {findInboxMessagesThunk} from '../../redux/messageThunks';
+import {AlertBox} from "../../components";
 
 /**
- * Creates a page that displays all the conversations for a given user
- * @returns {JSX.Element}
+ * Displays inbox or active chat based on the route. Uses the inbox messages fetched
+ * and stored in redux state to set up custom URLs for each conversation based on its ID.
  */
 const MessagesView = () => {
-  const [conversations, setConversations] = useState([]);
-  const [error, setError] = useState();
-  const authUser = useSelector((state) => state.user.data);
-
-  useEffect(() => {
-    const findMyConversations = async () => {
-      const res = await service.findInboxMessages(authUser.id);
-      if (res.error) {
-        return setError(
-          'We ran into an issue showing your conversations. Please try again later.'
-        );
-      }
-      setConversations(res);
-    };
-    findMyConversations();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <div>
-      <h1>Messages</h1>
-      {error && <AlertBox message={error} />}
-      {conversations && <Conversations conversations={conversations} />}
-    </div>
-  );
+    const inbox = useSelector((state) => state.messages.inbox);
+    const activeChat = useSelector((state) => state.messages.activeChat);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findInboxMessagesThunk());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return (
+        <div>
+            <h1>Messages</h1>
+            {inbox.length < 1 && <AlertBox message={'No conversations.'}/>}
+            <Routes>
+                <Route exact path={`/`} element={<Conversations conversations={inbox}/>}/>
+                <Route
+                    exact
+                    path={`/${activeChat.id}`}
+                    element={<Chat conversationId={activeChat.id}/>}
+                />
+                {inbox.length > 0
+                    ? inbox.map((message) => (
+                        <Route
+                            key={message.id}
+                            path={`/${message.conversation}`}
+                            element={<Chat conversationId={message.conversation}/>}
+                        ></Route>
+                    ))
+                    : null}
+            </Routes>
+        </div>
+    );
 };
+
 export default MessagesView;
