237c449c1e3a1fa6d91630b6c14bb819846b71f0
SpencerSolomon <spencersolomon6@gmail.com>
2022-04-23 13:59:42 -0400

    Cleaned up code in the services directory

--- src/services/follows-service.js
+++ src/services/follows-service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { processError } from './helpers';
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 const USERS_API = `${BASE_URL}/users`;
--- src/services/socket-config.js
+++ src/services/socket-config.js
@@ -8,20 +8,3 @@ export const socket = io(`${SOCKET_URL}`, {
     origin: CLIENT_URL,
   },
 });
-
-// const createSocket = () => {
-//   const socket = io(`${SOCKET_URL}`, {
-//     cors: {
-//       origin: CLIENT_URL,
-//     },
-//   });
-
-//   socket.on('connect', () => {
-//     console.log(`Client socket connect at id: ${socket.id}`);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log(`Client socket disconnected at id: ${socket.id}`);
-//   });
-//   return socket;
-// };
