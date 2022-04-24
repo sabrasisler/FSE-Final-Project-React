999813e99e176cfa52e3adbc5b372978c131ff4e
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-24 19:49:32 -0400

    fix notifications bug.

--- src/views/NotificationsView/NotificationsView.js
+++ src/views/NotificationsView/NotificationsView.js
@@ -1,53 +1,51 @@
-import { findNotificationsForUser, findAllNotifications } from '../../services/notifications-service';
-import React, {useEffect, useState} from 'react';
-import Notifications from "../../components/Notifications/index.js";
-import {useSelector} from "react-redux";
+import {
+  findNotificationsForUser,
+  findAllNotifications,
+} from '../../services/notifications-service';
+import React, { useEffect, useState } from 'react';
+import Notifications from '../../components/Notifications/index.js';
+import { useSelector } from 'react-redux';
 import { socket } from '../../services/socket-config';
 
 /**
- * Creates a page that displays all of the notifications for a given user 
+ * Creates a page that displays all of the notifications for a given user
  */
 const NotificationsView = () => {
-    const [notifications, setNotifications] = useState([]);
-    const [setError] = useState();
+  const [notifications, setNotifications] = useState([]);
+  const [setError] = useState();
 
-    const authUser = useSelector((state) => state.user.data);
+  const authUser = useSelector((state) => state.user.data);
 
-    // find all the notifications for a given user
-    const findMyNotifications = async () => {
-        const res = await findNotificationsForUser(authUser.id);
-        if (res.error) {
-            return setError(
-                'We ran into an issue showing your notifications. Please try again later.'
-            );
-        }
-        setNotifications(res);
-    };
+  // find all the notifications for a given user
+  const findMyNotifications = async () => {
+    const res = await findNotificationsForUser(authUser.id);
+    if (res.error) {
+      return setError(
+        'We ran into an issue showing your notifications. Please try again later.'
+      );
+    }
+    console.log('notifcations res', res);
+    setNotifications(res);
+  };
 
-    const listenForNewNotificationsOnSocket = async () => {
-        socket.emit('JOIN_ROOM'); // Server will assign room for user based on session.
-        socket.on('NEW_NOTIFICATION', () => {
-            // when a new notification is emitted to the room, find all of our notifications and refresh the state of our page
-            console.log('new notification from server!');
-            findMyNotifications();
-        });
-
-        socket.on('UPDATED_NOTIFICATION', () => {
-            // when a notification update is emitted to the room, find all of our notifications and refresh the state of our page
-            console.log('notification got updated!');
-            findMyNotifications();
-        })
-    };
-
-
-    useEffect(() => {
-        listenForNewNotificationsOnSocket();
+  const listenForNewNotificationsOnSocket = async () => {
+    socket.emit('JOIN_ROOM'); // Server will assign room for user based on session.
+    socket.on('NEW_NOTIFICATION', () => {
+      // when a new notification is emitted to the room, find all of our notifications and refresh the state of our page
+      console.log('new notification from server!');
+      findMyNotifications();
     });
-    return (
-        <div>
-            <h1>Notifications</h1>
-            <Notifications notifications={notifications}/>
-        </div>
-    );
+  };
+
+  useEffect(() => {
+    listenForNewNotificationsOnSocket();
+    findMyNotifications();
+  }, []);
+  return (
+    <div>
+      <h1>Notifications</h1>
+      <Notifications notifications={notifications} />
+    </div>
+  );
 };
 export default NotificationsView;
