79194449943d8d15e670a99a36ac5667d2a2af19
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-26 16:27:15 -0400

    fixed bug where notification component would not be marked as read in the ui without refreshing the page after a click

--- src/components/Notifications/notification.js
+++ src/components/Notifications/notification.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { markNotificationAsRead } from "./../../services/notifications-service";
 import { Link } from 'react-router-dom';
+
 /**
  * A component to render one notification.
  */
 const Notification = ({ notificationFromList }) => {
-
+    console.log(notificationFromList);
     const [notification, setNotification] = useState(notificationFromList);
     console.log(notification.id);
 
@@ -20,7 +21,7 @@ const Notification = ({ notificationFromList }) => {
     }
 
     const userActingLink = <Link to={`/tuiter/${notification.userActing.id}`} >@{notification.userActing && notification.userActing.username}</Link>
-   
+
     let boxColor;
     if (notification.read) {
         boxColor = 'black'
@@ -28,32 +29,40 @@ const Notification = ({ notificationFromList }) => {
         boxColor = '#0f2d3c'
     }
 
+    // function to update the notification as read both for display purposes and in the database
+    const readNotification = async () => {
+        if (!notification.read) {
+            markNotificationAsRead(notification.id);
+            let newNot = { createdAt: notification.createdAt, id: notification.id, read: true, type: notification.type, updatedAt: notification.updatedAt, userActing: notification.userActing, userNotified: notification.userNotified }
+            setNotification(newNot);
+        }
+    }
 
     return (
-        <li className={'p-2 list-group-item d-flex rounded-0'} onClick={() => markNotificationAsRead(notification.id)} ref={(el) => {
-            if (el) {
-                el.style.setProperty('background-color', boxColor, 'important');
-            }
-        
-        }}>
-            <div className= 'pe-2'>
-              {notification.userActing && (
-                <img
-                  src={
-                    notification.userActing.profilePhoto
-                      ? notification.userActing.profilePhoto
-                      : `../images/${notification.userActing.username}.jpg`
-                  }
-                  className='ttr-tuit-avatar-logo rounded-circle'
-                  alt='profile'
-                />
-              )}
-            </div>
-            <div>
-                {notification.read ? <span> {userActingLink} {notificationLink} </span> : <strong> {userActingLink} {notificationLink}</strong>}
-            </div>
-        </li>
+        <Link to={`/notifications`}>
+            <li className={'p-2 list-group-item d-flex rounded-0'} onClick={() => readNotification()} ref={(el) => {
+                if (el) {
+                    el.style.setProperty('background-color', boxColor, 'important');
+                }
+            }}>
+                <div className='pe-2'>
+                    {notification.userActing && (
+                        <img
+                            src={
+                                notification.userActing.profilePhoto
+                                    ? notification.userActing.profilePhoto
+                                    : `../images/${notification.userActing.username}.jpg`
+                            }
+                            className='ttr-tuit-avatar-logo rounded-circle'
+                            alt='profile'
+                        />
+                    )}
+                </div>
+                <div>
+                    {notification.read ? <span> {userActingLink} {notificationLink} </span> : <strong> {userActingLink} {notificationLink}</strong>}
+                </div>
+            </li>
+        </Link>
     );
 }
-
 export default Notification;
